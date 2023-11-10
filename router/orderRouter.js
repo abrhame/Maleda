e5e2f0c2c782a8beeb3210ae07a68ebf9655e72d
Abrham Lakew <99506193+abrhame@users.noreply.github.com>
2023-11-10 17:26:17 +0300

    Update orderRouter.js

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -3,7 +3,7 @@ const router = require("express").Router()
 
 const { addOrderValidation } = require("../utils/joiValidation");
 
-const { CreateOrder, viewOrders, viewOneOrder } = require("../controller/requestController");
+const { CreateOrder, viewOrders, viewOneOrder, PickedUp } = require("../controller/requestController");
 
 router.post("/", addOrderValidation, CreateOrder);
 
